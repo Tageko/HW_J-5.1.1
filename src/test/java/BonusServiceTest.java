31ecacb882c6c29a4e91c71baeb25e8c87bb044f
Tatyana Kopina <tatyanakopina@gmail.com>
2021-02-18 15:54:53 +0300

    add codes

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -5,7 +5,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class BonusServiceTest {
 
     @Test
-    void shouldCalculateUnregisteredAndUnderLimit() {
+    void shouldCalculateRegisteredAndUnderLimit() {
+
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldCalculateRegisteredAndOverLimit() {
+
+        BonusService service = new BonusService();
+
+        long amount = 36_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldCalculateUnRegisteredUnderLimit() {
 
         BonusService service = new BonusService();
 
@@ -19,41 +47,13 @@ class BonusServiceTest {
     }
 
     @Test
-    void shouldCalculateUnregisteredAndOverLimit() {
-
-        BonusService service = new BonusService();
-
-        long amount = 36_000_60;
-        boolean registered = false;
-        long expected = 30;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected,actual);
-    }
-
-    @Test
-    void shouldCalculateRegisteredAndZeroLimit() {
-
-        BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 0;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected,actual);
-    }
-
-    @Test
     void shouldCalculateUnregisteredAndZeroLimit() {
 
         BonusService service = new BonusService();
 
-        long amount = 1000_60;
+        long amount = 3000_60;
         boolean registered = false;
-        long expected = 0;
+        long expected = 30;
 
         long actual = service.calculate(amount, registered);
 
